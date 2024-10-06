71f703fc0f341d42ebb14e4dd7e8bc6e5bcd0691
Sebastien GERARD <sebastiengerard54@gmail.com>
2024-10-06 09:30:21 +0200

    Nettoyage de fichiers

--- scripts/read.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Charger le fichier CSV des clients pour vérifier les colonnes
-clients_df = pd.read_csv('../_sources/jeu-de-donnees-clients-66fed38c68779376654152.csv')
-
-# Afficher les colonnes présentes dans le fichier CSV
-print(clients_df.columns)
